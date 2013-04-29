202b9d5c67ff74a14838b6d0509753d09dd144f8
Zachary Crockett <towynlin@gmail.com>
2013-04-29 18:32:09 +0800

    Orb test pattern

--- src/application.c
+++ src/application.c
@@ -1,32 +1,107 @@
 #include "application.h"
 
-void setup()
-{
-	// runs once
+const int redLED = 3;
+const int greenLED = 5;
+const int blueLED = 6;
+const int whiteLED = 9;
 
-/*
-	// Serial Test
-	Serial.begin(9600);
-*/
+int i;
+
+void setup()  {
+  pinMode(redLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+  pinMode(blueLED, OUTPUT);
+  pinMode(whiteLED, OUTPUT);
 }
 
 void loop()
 {
-	// runs repeatedly
+  digitalWrite(redLED, LOW);
+  digitalWrite(greenLED, LOW);
+  digitalWrite(blueLED, LOW);
+  digitalWrite(whiteLED, LOW);
 
-/*
-	// Serial loopback test: what is typed on serial console
-	// using Hyperterminal/Putty should echo back on the console
-	if(Serial.available())
-	{
-		Serial.write(Serial.read());
-	}
-*/
+  delay(1000);
 
-/*
-	// Serial print test
-	Serial.print("Hello ");
-	Serial.println("Spark");
-	delay(500);
-*/
+  // Red
+  for(i=0; i<255; i++) {
+    analogWrite(redLED, i);
+    delay(4);
+  }
+
+  delay(1000);
+
+  // Green
+  for(i=0; i<255; i++) {
+    analogWrite(redLED, 255-i);
+    analogWrite(greenLED, i);
+    delay(4);
+  }
+
+  delay(1000);
+
+  // Blue
+  for(i=0; i<255; i++) {
+    analogWrite(greenLED, 255-i);
+    analogWrite(blueLED, i);
+    delay(4);
+  }
+
+  delay(1000);
+
+  // Red - Green
+  for(i=0; i<255; i++) {
+    analogWrite(redLED, i);
+    analogWrite(greenLED, i);
+    analogWrite(blueLED, 255-i);
+    delay(4);
+  }
+
+  delay(1000);
+
+  // Green - Blue
+  for(i=0; i<255; i++) {
+    analogWrite(redLED, 255-i);
+    analogWrite(blueLED, i);
+    delay(4);
+  }
+
+  delay(1000);
+
+  // Red - Blue
+  for(i=0; i<255; i++) {
+    analogWrite(redLED, i);
+    analogWrite(greenLED, 255-i);
+    delay(4);
+  }
+
+  delay(1000);
+
+  // Red - Green - Blue
+  for(i=0; i<255; i++) {
+    analogWrite(greenLED, i);
+    delay(4);
+  }
+
+    delay(1000);
+
+    // White
+  for(i=0; i<255; i++) {
+    analogWrite(blueLED, 255-i);
+    analogWrite(greenLED, 255-i);
+    analogWrite(redLED, 255-i);
+    analogWrite(whiteLED, i);
+    delay(4);
+  }
+
+  delay(1000);
+
+  for(i=0; i<255; i++) {
+    analogWrite(greenLED, i);
+    analogWrite(redLED, i);
+    analogWrite(blueLED, i);
+    delay(4);
+  }
+
+  delay(5000);
 }
