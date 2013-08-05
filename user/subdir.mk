a9590fa8be2d49fb0a93a7a5257d5607bb0b585d
David Middlecamp <dmiddlecamp@azavea.com>
2013-08-05 17:06:27 -0500

    adding safe default for when USER_DIR isn't specified

---
+++ user/subdir.mk
@@ -0,0 +1,7 @@
+########################################
+#   @author  Spark Application Team    #
+#   @version V1.0.0                    #
+#   @date    05-August-2013            #
+########################################
+
+# This file intentionally left blank.
