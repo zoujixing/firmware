965b7f3c7232ca8023b86ba5cb714d62fbea0625
David Middlecamp <dmiddlecamp@azavea.com>
2013-08-14 16:09:27 -0500

    Yeah! proof of concept!

--- tester/application.c
+++ tester/application.c
@@ -3,55 +3,77 @@
 void setup();
 void loop();
 
+void allOff();
+void handlePinMessage(int32_t pin);
+
 int state = 0;
-int theDelay = 500;
+//int theDelay = 500;
+
+int pinRange_start = (int)'0';
+int pinRange_end = (int)'9';
 
 
 void setup()
 {
-	// Serial Test
+	// light em up.
 	pinMode(D0, OUTPUT);
-	Serial.begin(9600);	
-	digitalWrite(D0, HIGH);
+	pinMode(D1, OUTPUT);
+	pinMode(D2, OUTPUT);
+	pinMode(D3, OUTPUT);
+	pinMode(D4, OUTPUT);
+	pinMode(D5, OUTPUT);
+	pinMode(D6, OUTPUT);	
+	pinMode(D7, OUTPUT);
 	
-	/*
-	int i=0;
-	for(i=0;i<1000;i++) {
-		Serial.println("THE PAIN! OH THE PAIN OF IT ALL! ");
-		
-		digitalWrite(D0, (state) ? HIGH : LOW);	
-		state = (state) ? 0 : 1;
-		
-		Delay(250);
-	}
-	*/	
+	//
+	Serial.begin(9600);	
+	digitalWrite(D0, HIGH);	
 }
+
 
 void loop()
 {
-	//D0 = 0, D1 = 1, D2 = 2, D3 = 3, D4 = 4, D5 = 5, D6 = 6, D7 = 7
-	
-
-
-	Serial.println("I know right?");		
-	digitalWrite(D0, (state) ? HIGH : LOW);	
-	state = (state) ? 0 : 1;		
-	Delay(theDelay);
-	
 	if (Serial.available()) {
 		int32_t c = Serial.read();
-		if (c == (int)'+') {
-			theDelay = theDelay + 100;
+
+		
+		if ((c >= pinRange_start) && (c <= pinRange_end)) {
+			//if we should receive a byte value in the range of '0'-'9'
+			//lets assume they would like us to turn off all the pins, and turn on just that pin.			
+			handlePinMessage(c);
+			Serial.println("You got it.");
 		}
-		else if (c == (int)'-') {
-			theDelay = theDelay - 100;
+		else {
+			//other commands...
+			Serial.println("I didn't understand that.");
 		}
 	}
 }
 
 void handlePinMessage(int32_t pin) {
-
+	//D0 = 0, D1 = 1, D2 = 2, D3 = 3, D4 = 4, D5 = 5, D6 = 6, D7 = 7
+	pin = pin - (int)pinRange_start;
+	if ((pin < 0) || (pin > 7)) {
+		//shouldn't get here.
+		Serial.println("Uh oh, weird pin value came through");
+		return;
+	}
+	
+	allOff();
+	digitalWrite(pin, HIGH);	
+	//digitalWrite(D0, (state) ? HIGH : LOW);
+	//state = (state) ? 0 : 1;
 }
 
+void allOff() {
+	digitalWrite(D0, LOW);
+	digitalWrite(D1, LOW);
+	digitalWrite(D2, LOW);
+	digitalWrite(D3, LOW);
+	digitalWrite(D4, LOW);
+	digitalWrite(D5, LOW);
+	digitalWrite(D6, LOW);
+	digitalWrite(D7, LOW);
+}
 
 
