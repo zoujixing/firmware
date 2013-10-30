8134e075aee6a3ae04219e7a918418081d419f3e
David Middlecamp <middleca@gmail.com>
2013-10-30 15:00:48 -0500

    enable jtag, so we can erase the chip to force us back into DFU

--- inc/main.h
+++ inc/main.h
@@ -48,7 +48,7 @@ extern "C" {
  * Note that once the JTAG IOs are disabled, the connection with the host debugger
  * is lost and cannot be re-established as long as the JTAG IOs remain disabled.
  */
-#define SWD_JTAG_DISABLE
+//#define SWD_JTAG_DISABLE
 
 /*
  * Use Independent Watchdog to force a system reset when a software error occurs
