66922d654f7a23ad507b9632ebadb7e3b439f4ed
Matthew McGowan <mat.mcgowan+github@gmail.com>
2015-07-21 11:35:09 +0200

    using real pinmap constants with the GCC compile.

--- hal/inc/pinmap_hal.h
+++ hal/inc/pinmap_hal.h
@@ -33,7 +33,7 @@
 
 #ifdef __cplusplus
 extern "C" {
-#endif    
+#endif
     
 /* Exported types ------------------------------------------------------------*/
 
@@ -48,7 +48,7 @@ typedef enum PinMode {
   AF_OUTPUT_DRAIN,    //Used internally for Alternate Function Output Drain(I2C etc). External pullup resistors required.
   AN_INPUT,           //Used internally for ADC Input
   AN_OUTPUT,          //Used internally for DAC Output
-  PIN_MODE_NONE=0xFF          
+  PIN_MODE_NONE=0xFF
 } PinMode;
 
 typedef enum {
@@ -70,6 +70,7 @@ STM32_Pin_Info* HAL_Pin_Map(void);
 /*
 * Pin mapping. Borrowed from Wiring
 */
+#if PLATFORM_ID!=3
 
 #define TOTAL_PINS 21
 #define TOTAL_ANALOG_PINS 8
@@ -141,6 +142,81 @@ STM32_Pin_Info* HAL_Pin_Map(void);
 // DAC pins on Photon
 #define DAC1 16
 #define DAC2 13
+
+#else
+const pin_t TOTAL_PINS = 21;
+const pin_t TOTAL_ANALOG_PINS = 8;
+const pin_t FIRST_ANALOG_PIN = 10;
+
+
+const pin_t D0 = 0;
+const pin_t D1 = 1;
+const pin_t D2 = 2;
+const pin_t D3 = 3;
+const pin_t D4 = 4;
+const pin_t D5 = 5;
+const pin_t D6 = 6;
+const pin_t D7 = 7;
+
+#if PLATFORM_ID<3
+const pin_t LED1 = LED_USER;
+#endif
+
+const pin_t A0 = 10;
+const pin_t A1 = 11;
+const pin_t A2 = 12;
+const pin_t A3 = 13;
+const pin_t A4 = 14;
+const pin_t A5 = 15;
+const pin_t A6 = 16;
+
+// WKP pin is also an ADC on Photon
+const pin_t A7 = 17;
+
+// RX and TX pins are also ADCs on Photon
+const pin_t A8 = 18;
+const pin_t A9 = 19;
+
+const pin_t RX = 18;
+const pin_t TX = 19;
+
+const pin_t BTN = 20;
+
+// WKP pin on Photon
+const pin_t WKP = 17;
+
+// Timer pins
+
+const pin_t TIMER2_CH1 = 10;
+const pin_t TIMER2_CH2 = 11;
+const pin_t TIMER2_CH3 = 18;
+const pin_t TIMER2_CH4 = 19;
+
+const pin_t TIMER3_CH1 = 14;
+const pin_t TIMER3_CH2 = 15;
+const pin_t TIMER3_CH3 = 16;
+const pin_t TIMER3_CH4 = 17;
+
+const pin_t TIMER4_CH1 = 1;
+const pin_t TIMER4_CH2 = 0;
+
+// SPI pins
+
+const pin_t SS   = 12;
+const pin_t SCK  = 13;
+const pin_t MISO = 14;
+const pin_t MOSI = 15;
+
+// I2C pins
+
+const pin_t SDA  = 0;
+const pin_t SCL  = 1;
+
+// DAC pins on Photon
+const pin_t DAC1 = 16;
+const pin_t DAC2 = 13;
+
+#endif
 
 #define TIM_PWM_COUNTER_CLOCK_FREQ 24000000 //TIM Counter clock = 24MHz
 #define TIM_PWM_FREQ 500 //500Hz
